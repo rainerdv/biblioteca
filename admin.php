5bc8c4ecc52b222ef0656591abf04ddd526e3724
mario <mariodelvalle514@gmail.com>
2023-08-21 11:45:19 -0600

    hola5

--- admin.php
+++ admin.php
@@ -127,7 +127,7 @@
 		<br />
 		<nav class = "navbar navbar-default navbar-fixed-bottom">
 			<div class = "container-fluid">
-				<label class = "navbar-text pull-right">Sistema de Biblioteca | Platea21 &copy; Todos los Derechos Reservados 2018</label>
+
 			</div>
 		</nav>
 	</body>
